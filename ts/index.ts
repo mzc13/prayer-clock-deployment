b836ee2b45e9bf9bd41e77c290ea2430875919eb
Muhmmad Choudhary <cho.muhmmad@gmail.com>
2021-01-16 02:39:34 +0000

    Made config file to be used for admin page

--- ts/index.ts
+++ ts/index.ts
@@ -1,16 +1,38 @@
 import express from "express";
+import jsonfile from "jsonfile";
 
 const app = express();
 
 const host = "0.0.0.0";
 const port = 80;
 
-app.use(express.static("static"));
+// const APP_PATH = process.env.APP_PATH!;
+const APP_PATH = "/home/node/prayer-clock-deployment";
+
+app.use(express.static(APP_PATH + "/static"));
 app.use(express.json());
 
-// app.get("/", (req, res) => {
-//   res.send("Kiosk App Running");
-// });
+app.post("/admin", (req, res) => {
+  res.send("App running");
+});
+
+type ClockConfig = {
+  admin_username: string,
+  admin_password: string,
+  fajr_iqamah: string,
+  dhuhr_iqamah: string,
+  asr_iqamah: string,
+  maghrib_iqamah: string,
+  isha_iqamah: string,
+  jummah_1: string,
+  jummah_2: string | undefined,
+  jummah_3: string | undefined,
+  jummah_4: string | undefined
+}
+app.get("/admin", async (req, res) => {
+  let jfile: ClockConfig = await jsonfile.readFile(APP_PATH + '/clock_config.json');
+  res.send(jfile);
+});
 
 app.listen(port, host, () => {
   console.log(`App listening at http://${host}:${port}`);
